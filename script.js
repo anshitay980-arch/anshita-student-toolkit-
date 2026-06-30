4d1b64f180fa97b6f46950dbdc61ff3d15d21351
anshitay980-arch <anshitay980@gmail.com>
2026-06-30 22:40:51 +0530

    Add input focus and task row elements

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const task input=document.getElementById('taskinput');
    const addTaskBtn=document.getElementById('addtaskBtn');
+addTaskBtn.addEventListener('click',()=>{taskinput.focus();
+    const newRow=document.createElement('tr');const taskText =taskinput.value;});
