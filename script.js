69a604938e92620b555864910f2fc6e9dd9eb561
anshitay980-arch <anshitay980@gmail.com>
2026-07-03 15:59:55 +0530

    Update2 script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const task input=document.getElementById('taskinput');
    const addTaskBtn=document.getElementById('addtaskBtn');
 addTaskBtn.addEventListener('click',()=>{taskinput.focus();
-    const newRow=document.createElement('tr');const taskText =taskinput.value;});
+    const newRow=document.createElement('tr');const taskText =taskinput.value;
+const checkbox=document.createElement('input');
+    checkbox.type='checkbox';
+    const checkboxcell=document.createElement('td');
+   checkboxcell.appendChild(checkbox);});
