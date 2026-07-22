7998ee22a8584846dbdb6a16becd5d75d0bec988
anshitay980-arch <anshitay980@gmail.com>
2026-07-22 17:54:31 +0530

    ​Build renderTask function for dynamic task card creation

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const addTaskbtn = document.getElementById('addBtn');
 const taskList = document.getElementById('tasklist');
 const clearBtn = document.getElementById('clearBtn');
 let tasks=JSON.parse(localStorage.getItem('fresh_tasks_vault')) || [];
+function renderTask(taskText){
+const card = document.createElement('div');
+card.classList.add('task-card');
+const textSpan = document.createElement('span');
+ textSpan.textContent=taskText;
+ const checkbox = document.createElement('input');
+checkbox.type='checkbox';
+card.appendChild(textSpan);
+card.appendChild(checkbox);
+taskList.appendChild(card);
+}
+
+
