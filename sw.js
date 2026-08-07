cd445491524b7db7679984c067978d9ac1949697
anshitay980-arch <anshitay980@gmail.com>
2026-08-07 22:59:26 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,11 @@
-const CACHE_NAME = 'toolkit-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './Dashboard.html',
-  './todo.html',
-  './Unit converter.html',
-  './todo.css',
-  './converter.css',
-  './script.js'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
 });
