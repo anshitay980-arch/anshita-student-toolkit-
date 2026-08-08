97a775715f030159551e3f79263edd635e38c332
anshitay980-arch <anshitay980@gmail.com>
2026-08-08 23:35:59 +0530

    Create converter.js & learned DOM binding , connecting html elements to javascript and event handling

---
+++ converter.js
@@ -0,0 +1,7 @@
+const select =document.getElementById('categorySelect');
+const inputtype = document.getElementById('inputValue');
+const fromUnit =document.getElementById('fromUnit');
+const toUnit = document.getElementById('toUnit');
+const button = document.getElementById('convertBtn');
+const result = document.getElementById('resultDisplay');
+convertBtn.addEventListener('click',function(){alert('connected!');});
