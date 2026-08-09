038c9592d3313b39c043e056877307f4f6b14a23
anshitay980-arch <anshitay980@gmail.com>
2026-08-09 23:07:19 +0530

    Update converter.js

--- converter.js
+++ converter.js
@@ -5,3 +5,23 @@ const toUnit = document.getElementById('toUnit');
 const button = document.getElementById('convertBtn');
 const result = document.getElementById('resultDisplay');
 convertBtn.addEventListener('click',function(){alert('connected!');});
+let valueInMetres;
+let finalResult;
+convertBtn.addEventListener('click',function(){
+const userTypedText=inputValue.value;
+const selectedFrom=fromUnit.value;
+const selectedTo=toUnit.value;
+const numericValue=parseFloat(inputValue.value);
+  if(selectedFrom==="m")
+  { valueInMetres=numericValue;}
+  if(selectedFrom==="km")
+ { valueInMetres=numericValue*1000;}
+ if(selectedFrom==="ft")
+  {valueInMetres=numericValue*0.3048;}
+  if(selectedTo==="m")
+  {valueInMetres=numericValue;}
+  if(selectedTo==="km")
+  {valueInMetres=numericValue*1000;}
+  if(selectedTo==="ft")
+  {valueInMetres=numericValue*0.3048;}alert("Result:"+finalResult);
+});
