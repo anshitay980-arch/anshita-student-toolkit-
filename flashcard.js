f16f748c32e715ec62f180a16140700de23e9a33
anshitay980-arch <anshitay980@gmail.com>
2026-09-23 23:52:29 +0530

    Update flashcard.js

--- flashcard.js
+++ flashcard.js
@@ -3,7 +3,15 @@ const flipbtn =document.getElementById("flipbtn");
 flipbtn.addEventListener('click',function  (){
 flashcard.classList.toggle('flipped')
 });
+deck=[
+  {front:"Your term or question1",back:"Your definition or answer1"},
+  {front:"Your term or question2",back:"Your definitionconst or answer2"}
+];
+let currentIndex=0;
+const cardFront=document.getElementById("cardFront");
+const cardBack=document.getElementById("cardBack");
 
+  
 
 
   
